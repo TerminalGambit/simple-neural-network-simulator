84a742147b3ac1f1c94493633b21bb7a738ff449
TerminalGambit <jackmassey.monaco@outlook.com>
2024-06-21 22:42:02 +0200

    added the getter and setter for the new property

--- src/BasicNeuron.java
+++ src/BasicNeuron.java
@@ -1,6 +1,33 @@
 class BasicNeuron implements Neuron {
-    -inputValue: double
-    -outputValue: double
-    +fire(): void
-    +calculateOutput(): double
+    private double inputValue;
+    private double outputValue;
+
+    public BasicNeuron(double inputValue, double outputValue) {
+        this.inputValue = inputValue;
+        this.outputValue = outputValue;
+    }
+
+    public void fire() {
+        outputValue = calculateOutput();
+    }
+    
+    public double calculateOutput() {
+        return inputValue;
+    }
+
+    public double getOutputValue() {
+        return outputValue;
+    }
+
+    public void setInputValue(double inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public void setOutputValue(double outputValue) {
+        this.outputValue = outputValue;
+    }
+
+    public double getInputValue() {
+        return inputValue;
+    }
 }
