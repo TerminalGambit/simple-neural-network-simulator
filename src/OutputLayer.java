71286acad67aaa76d6b530cbdcd3ab5b2ae506b3
TerminalGambit <jackmassey.monaco@outlook.com>
2024-06-21 22:47:20 +0200

    let's go!

---
+++ src/OutputLayer.java
@@ -0,0 +1,14 @@
+/* class OutputLayer extends Layer {
+    +forwardPass(): void
+} */
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+public class OutputLayer extends Layer {
+    public void forwardPass() {
+        for (Neuron neuron : neurons) {
+            neuron.fire();
+        }
+    }
+}
